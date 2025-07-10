ec8b73a76f873dd1c80c89eca7b22f3df8a743c5
CryptoSnipeAI <vasily2001@yahoo.com>
2025-07-10 18:24:31 +0300

    Update features.py

--- features.py
+++ features.py
@@ -1,15 +1,22 @@
 import pandas as pd
 from ta.trend import MACD, CCIIndicator, ADXIndicator, EMAIndicator, TRIXIndicator, SMAIndicator
 from ta.momentum import RSIIndicator, StochasticOscillator
-from ta.volume import OnBalanceVolume
+
+def calculate_obv(df):
+    obv = [0]
+    for i in range(1, len(df)):
+        if df['close'].iloc[i] > df['close'].iloc[i - 1]:
+            obv.append(obv[-1] + df['volume'].iloc[i])
+        elif df['close'].iloc[i] < df['close'].iloc[i - 1]:
+            obv.append(obv[-1] - df['volume'].iloc[i])
+        else:
+            obv.append(obv[-1])
+    return pd.Series(obv, index=df.index)
 
 def prepare_features(df):
     df = df.copy()
-
-    # Убедимся, что нет пропущенных значений
     df.dropna(inplace=True)
 
-    # Простые индикаторы
     df['rsi'] = RSIIndicator(close=df['close']).rsi()
     df['stoch_k'] = StochasticOscillator(high=df['high'], low=df['low'], close=df['close']).stoch()
     df['stoch_d'] = StochasticOscillator(high=df['high'], low=df['low'], close=df['close']).stoch_signal()
@@ -19,12 +26,6 @@ def prepare_features(df):
     df['ema'] = EMAIndicator(close=df['close']).ema_indicator()
     df['sma'] = SMAIndicator(close=df['close']).sma_indicator()
     df['trix'] = TRIXIndicator(close=df['close']).trix()
-    df['obv'] = OnBalanceVolume(close=df['close'], volume=df['volume']).on_balance_volume()
+    df['obv'] = calculate_obv(df)
 
-    # Возвращаем только нужные колонки
-    feature_columns = [
-        'rsi', 'stoch_k', 'stoch_d', 'macd', 'cci', 'adx',
-        'ema', 'sma', 'trix', 'obv'
-    ]
-
-    return df[feature_columns].dropna()
+    return df[['rsi', 'stoch_k', 'stoch_d', 'macd', 'cci', 'adx', 'ema', 'sma', 'trix', 'obv']].dropna()
